0c51433adf5c43313a9b9a892d34a62a1b3b27b9
daxus4 <davide.raffaellii@gmail.com>
2021-01-23 19:00:28 +0100

    Create Exam class and relative test class

---
+++ lib/exam/exam.dart
@@ -0,0 +1,10 @@
+class Exam {
+
+  final String name;
+  final int cfu;
+  final int mark;
+  final bool cumLaude;
+
+  Exam(this.name, this.cfu, this.mark, this.cumLaude);
+
+}
---
+++ test/exam_test/exam_test.dart
@@ -0,0 +1,13 @@
+//Test of Exam class
+
+import 'package:graduation_grade/exam/exam.dart';
+
+final examName = "Math";
+final examCfu = 10;
+final examMark = 27;
+final cumLaude = true;
+
+void main() {
+  var exam = Exam(examName, examCfu, examMark, cumLaude);
+
+}
