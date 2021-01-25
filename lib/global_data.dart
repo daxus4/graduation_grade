9a2125b461f9894e7615d13861134e9cfb0f119e
daxus4 <davide.raffaellii@gmail.com>
2021-01-25 18:07:10 +0100

    Create global_data.dart which contain global app information

---
+++ lib/global_data.dart
@@ -0,0 +1,15 @@
+// Class where are stored all information of the application that should be
+// global
+import 'package:flutter/material.dart';
+
+class GlobalData {
+
+  static final appName = "Graduation Mark";
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+  );
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.black,
+  );
+
+}
