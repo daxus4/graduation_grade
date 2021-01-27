66d234a3e1d1d22ca9700f8082114a318e127e49
daxus4 <davide.raffaellii@gmail.com>
2021-01-27 16:02:50 +0100

    create functions that convert exam and passedexam in Map, due to be inserted in a database made with sqflite

--- lib/exam/exam.dart
+++ lib/exam/exam.dart
@@ -7,4 +7,22 @@ class Exam {
 
   Exam(this.name, this.cfu);
 
+  Map<String, dynamic> toMap() {
+    return {
+      'name' : name,
+      'cfu' : cfu,
+      'mark' : 0,
+      'cumLaude' : false,
+    };
+  }
+
+  Exam.fromMapObject(Map<String, dynamic> examMap) :
+      name = examMap['name'],
+      cfu = examMap['cfu'];
+
+  @override
+  String toString() {
+    return 'Exam{name: $name, cfu: $cfu}';
+  }
+
 }
--- lib/exam/passed_exam.dart
+++ lib/exam/passed_exam.dart
@@ -1,18 +1,35 @@
-
 import 'package:graduation_grade/exam/exam.dart';
 import 'package:graduation_grade/exception/laude_exception.dart';
 import 'package:graduation_grade/exception/mark_exception.dart';
 
 class PassedExam extends Exam {
-
   final int mark;
   final bool cumLaude;
 
-  PassedExam(String name, int cfu, this.mark, this.cumLaude) : super(name, cfu) {
-    if(this.mark < 18 || this.mark > 30)
-      throw MarkException(mark);
-    if(this.cumLaude && mark != 30)
-      throw LaudeException(mark);
+  PassedExam(String name, int cfu, this.mark, this.cumLaude)
+      : super(name, cfu) {
+    if (this.mark < 18 || this.mark > 30) throw MarkException(mark);
+    if (this.cumLaude && mark != 30) throw LaudeException(mark);
   }
 
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'cfu': cfu,
+      'mark': mark,
+      'cumLaude': cumLaude,
+    };
+  }
+
+  PassedExam.fromMapObject(Map<String, dynamic> examMap)
+      : mark = examMap['mark'],
+        cumLaude = examMap['cumLaude'],
+        super.fromMapObject(examMap);
+
+  @override
+  String toString() {
+    String s = super.toString();
+    return s.substring(0, s.length - 1) + ', mark: $mark, cumLaude: $cumLaude}';
+  }
 }
