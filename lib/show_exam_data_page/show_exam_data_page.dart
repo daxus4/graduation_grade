6ccb82a497d57e61d1c10ba87ec05489232ffcb0
daxus4 <davide.raffaellii@gmail.com>
2021-02-09 18:12:22 +0100

    set everything on showexamdatapage

--- lib/show_exam_data_page/show_exam_data_page.dart
+++ lib/show_exam_data_page/show_exam_data_page.dart
@@ -10,7 +10,6 @@ class ShowExamDataPage extends StatefulWidget {
 }
 
 class ShowExamDataPageState extends State<ShowExamDataPage> {
-
   @override
   Widget build(BuildContext context) {
     final Exam exam = ModalRoute.of(context).settings.arguments;
@@ -34,53 +33,76 @@ class ShowExamDataPageState extends State<ShowExamDataPage> {
             SizedBox(
               height: 16,
             ),
-            Row(
-              children: <Widget>[
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).primaryColor, // background
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (_) => AlertDialog(
-                            title: Text("Delete"),
-                            content: Text("Are you sure to delete this exam?"),
-                            actions: <Widget>[
-                              TextButton(
-                                  child: Text("No"),
-                                  onPressed: () { 
-                                    Navigator.of(context).pop();
-                                  },
-                              ),
-                              TextButton(
-                                child: Text("Yes"),
-                                onPressed: () async {
-                                  await ExamRepository.deleteExam(exam);
-                                  Navigator.popUntil(context,ModalRoute.withName('/'));
-                                },
-                              ),
-                            ],
-                          ),
-                      );
-                    },
-                    child: Text(
-                      "Delete",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor, // background
-                  ),
-                  onPressed: () {  },
-                  child: Text(
-                    "Modify",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ]
+            Text(
+              exam.isTaken() ? 'Mark: ${exam.getMark()}' : 'Mark: none',
+              style: TextStyle(
+                color: exam.isTaken() ? Colors.black : Colors.grey,
+              ),
             ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Laude:',
+                  style: TextStyle(
+                    color: exam.isTaken() ? Colors.black : Colors.grey,
+                  ),
+                ),
+                Checkbox(
+                  value: exam.isTaken() ? exam.getLaude() : false,
+                  onChanged: (bool value) {  },
+                ),
+              ],
+            ),
+            Row(children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor, // background
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      title: Text("Delete"),
+                      content: Text("Are you sure to delete this exam?"),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text("No"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        TextButton(
+                          child: Text("Yes"),
+                          onPressed: () async {
+                            await ExamRepository.deleteExam(exam);
+                            Navigator.popUntil(
+                                context, ModalRoute.withName('/'));
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: Text(
+                  "Delete",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor, // background
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Modify",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ]),
           ],
         ),
       ),
