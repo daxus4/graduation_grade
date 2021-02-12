36f9c61ec7e10f4fee1d044b452d67f0c5b21867
daxus4 <davide.raffaellii@gmail.com>
2021-02-13 00:16:42 +0100

    added some function on exam controller

--- lib/exam_controller/exam_controller.dart
+++ lib/exam_controller/exam_controller.dart
@@ -6,13 +6,25 @@ class ExamController {
 
   ExamController(this._exams);
 
-  bool isThere(Exam e) => _exams.any((exam) => exam.getName() == e.getName());
+  bool isThere(Exam e) => _exams.contains(e);
 
   void addExam(Exam e) {
     if (isThere(e))
       throw AlreadyPresentExamException(e.getName());
     _exams.add(e);
   }
+
+  void removeExam(Exam e) {
+    if(isThere(e))
+      _exams.remove(e);
+  }
+
+  void takeExam(Exam e, int mark, bool laude) {
+    _exams.remove(e);
+    _exams.add(Exam.taken(e.getName(), e.getCfu(), mark, laude));
+  }
+
+  void deleteAll() => _exams.clear();
 
 }
 
