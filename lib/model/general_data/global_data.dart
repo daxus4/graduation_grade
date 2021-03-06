edb80cda0ec8512a6716890b5de30fec618b9068
daxus4 <davide.raffaellii@gmail.com>
2021-03-06 19:05:46 +0100

    change name

--- lib/model/general_data/global_data.dart
+++ lib/model/general_data/global_data.dart
@@ -3,9 +3,9 @@
 
 class GlobalData {
 
-  static final appName = "Graduation Mark";
+  static final appName = "Graduation Grade";
 
-  static final dbName = 'graduation_mark_db';
+  static final dbName = 'graduation_grade_db';
   static final examTableName = 'exam_table';
   static final examNameAttribute = 'exam';
   static final examCfuAttribute = 'cfu';
